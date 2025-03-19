fed4d580f8b5b0ecb8e6637e8d603ea9f6d92ff1
Mayank Chourasia <mayank.chourasia77@gmail.com>
2025-03-19 15:16:19 +0530

    Disabled debug mode for production

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ def batter_runs():
 import os
 
 port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
-app.run(host="0.0.0.0", port=port, debug=True)
+app.run(host="0.0.0.0", port=port, debug=False)
